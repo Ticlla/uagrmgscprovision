06b9f7bd831e476aaa7702ebdda1ba44482c9be6
Alcides Ticlla <alcidesticlla@cloud.upwork.com>
2021-02-25 19:53:48 -0400

    updates adds sbt installer by command

--- gcs-setup/recipes/gscrecipe.rb
+++ gcs-setup/recipes/gscrecipe.rb
@@ -12,8 +12,17 @@ package 'scala' do
 	action :install
 end
 
-package 'sbt' do
-	action :install
+# Doesn't Work it seems like chef don't have this package
+# package 'sbt' do
+# 	action :install
+# end
+
+execute 'sbt-download' do
+    command 'curl https://bintray.com/sbt/rpm/rpm | sudo tee /etc/yum.repos.d/bintray-sbt-rpm.repo'
+end
+
+execute 'sbt-install' do
+    command 'sudo dnf install sbt'
 end
 
 package 'docker' do
