6cc1902258df5e987cfd7b2948eb0f640e29b620
Alcides Ticlla <alcidesticlla@cloud.upwork.com>
2021-03-02 17:04:13 -0400

    comments config files

--- posgresql-setup/recipes/posgresqlsetup.rb
+++ posgresql-setup/recipes/posgresqlsetup.rb
@@ -14,15 +14,15 @@ execute 'Installing posgrsql server' do
 end
 
 
-template '/var/lib/pgsql/data/pg_hba.conf' do
-	source 'pg_hba.conf.erb'
-	action :create
-end
+# template '/var/lib/pgsql/data/pg_hba.conf' do
+# 	source 'pg_hba.conf.erb'
+# 	action :create
+# end
 
-template '/var/lib/pgsql/data/postgresql.conf' do
-	source 'postgresql.conf.erb'
-	action :create
-end
+# template '/var/lib/pgsql/data/postgresql.conf' do
+# 	source 'postgresql.conf.erb'
+# 	action :create
+# end
 
 
 execute 'set up initdb' do
