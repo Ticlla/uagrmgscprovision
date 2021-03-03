b39981132dbbd90403ced91aa48db5ce004d83db
Alcides Ticlla <alcidesticlla@cloud.upwork.com>
2021-03-02 20:10:52 -0400

    updates recipe

--- posgresql-setup/recipes/config.rb
+++
@@ -1,9 +0,0 @@
-template '/var/lib/pgsql/data/pg_hba.conf' do
-	source 'pg_hba.conf.erb'
-	action :create
-end
-
-template '/var/lib/pgsql/data/postgresql.conf' do
-	source 'postgresql.conf.erb'
-	action :create
-end
